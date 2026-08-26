1d4c7a4f1bc9f490ba984eacba168ca4def3db1c
Mohammadparsa Tabasi <tabasiparsa2007@gmail.com>
2026-08-26 11:40:47 +0300

    hello

---
+++ module3ex5.py
@@ -0,0 +1,19 @@
+talents = int(input("Enter talents: "))
+
+pounds = int(input("Enter pounds: "))
+
+lots = float(input("Enter lots: "))
+
+
+
+total_grams = (talents * 20 * 32 + pounds * 32 + lots) * 13.3
+
+
+
+kilograms = int(total_grams // 1000)
+
+remaining_grams = total_grams % 1000
+
+print("The weight in modern units:")
+
+print(f"{kilograms} kilograms and {remaining_grams:.2f} grams.")
---
+++ module3ex6.py
@@ -0,0 +1,7 @@
+import random
+
+code3 = "".join(str(random.randint(0, 9)) for _ in range(3))
+code4 = "".join(str(random.randint(1, 6)) for _ in range(4))
+
+print("3-digit code:", code3)
+print("4-digit code:", code4)
