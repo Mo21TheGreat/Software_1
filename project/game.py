4022873c131b93ffe9c14da30373f49d9944d042
Mohammadparsa Tabasi <tabasiparsa2007@gmail.com>
2026-09-06 23:44:18 +0300

    game

--- project/game.py
+++ project/game.py
@@ -1,5 +1,35 @@
 name = input("Write your beautiful name: ")
 age = int(input("Enter your age: "))
 
+if age < 12:
+    print("You are a minor. The program will now shut down. ")
+else:
+    print("Welcome to the game!")
+
+    while True:
+        print("\n--- MAIN MENU ---")
+        print("1. start")
+        print("2. explore")
+        print("3. inventory")
+        print("4. lopeta")
+
+        command = input("Enter command: ")
+
+        if command == "start":
+            print("The game has started!")
+
+        elif command == "explore":
+            print("You explore the mysterious forest.")
+
+        elif command == "inventory":
+            print("Your inventory is empty.")
+
+        elif command == "lopeta":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Unknown command.")
+
 print("Name:",name)
 print("Age:",age)
